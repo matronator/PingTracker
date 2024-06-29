3dbaa8503b8af27f4073d3520e7f5d1bee25ac14
Matronator <5470780+matronator@users.noreply.github.com>
2024-06-29 05:17:55 +0200

    test

--- test/pinga.test.ts
+++ test/pinga.test.ts
@@ -1,9 +1,9 @@
 import { assert, describe, it } from "vitest";
-import { LinkPing } from "../src/index";
+import { PingA } from "../src/index";
 
 describe("construct", () => {
     it("Set URL list", () => {
-        const lp = new LinkPing("https://example.com");
+        const lp = new PingA("https://example.com");
         assert((lp.urls = ["https://example.com"]), "URL list not same");
     });
 });
