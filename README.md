91bc7f7287bd48155f30700403f073cc10a9aa0e
Matronator <5470780+matronator@users.noreply.github.com>
2024-07-03 01:43:55 +0200

    update

--- README.md
+++ README.md
@@ -1,18 +1,43 @@
 # Ping Tracker
 
+![Ping Tracker logo](.github/ping-tracker.png)
+
+![Version](https://badgen.net/npm/v/ping-tracker)
+![License](https://img.shields.io/github/license/matronator/PingTracker.svg)
+![Commits](https://badgen.net/github/commits/matronator/PingTracker)
+![Issues](https://img.shields.io/github/issues/matronator/PingTracker.svg)
+![Typescript](https://img.shields.io/badge/TypeScript-007ACC?style=default&logo=typescript&logoColor=white)
+![Follow](https://img.shields.io/github/followers/matronator.svg?style=social&label=Follow&maxAge=2592000)
+
 Ping Tracker is a tiny library that adds `ping` attribute to all `<a>` tags on the site with customizable list of URLs.
 
 ## Features
 
--   Add `ping` attribute to all `<a>` tags automatically
--   Customize the content of the `ping` tag (list of URLs)
--   Tiny library
--   Written in TypeScript
+- 🏓 Add `ping` attribute to all `<a>` tags automatically
+- 👀 Watch for changes using `MutationObserver` to modify newly added links
+- 📝 Customize the content of the `ping` tag (list of URLs)
+- ⚡️ Tiny library
+- 🔥 Written in TypeScript
 
-## Usage
-
-### Installation
+## Installation
 
 ```
 npm i ping-tracker
+```
+
+## Usage
+
+```js
+import { PingTracker } from 'ping-tracker';
+
+const pt = new PingTracker('https://analytics.example.com');
+
+// Or with options
+
+const pt = new PingTracker('https://analytics.example.com', {
+    hrefToQuery: false,
+    onlyExternal: false,
+    watchDOM: true,
+    elementsToWatch: document.body,
+});
 ```
--- src/ping-tracker.ts
+++ src/ping-tracker.ts
@@ -1,13 +1,31 @@
 // Add sendBeacon fallback
 
+/**
+ * @interface PingTrackerConfig
+ * @field {boolean} hrefToQuery If `true` appends the url inside the link's href to the ping's URL query (ie.: `?href=https://example.com`)
+ * @field {boolean} onlyExternal If `true` only external links will be modified (defaults to `false`)
+ * @field {Element | Element[] | Node | Node[] | NodeList | NodeListOf<Element|HTMLElement>} elementsToWatch Element or elements to watch for changes (defaults to `document.body`)
+ * @field {boolean} watchDOM Watch DOM changes to modify newly created <a> tags (defaults to `true`)
+ */
 export interface PingTrackerConfig {
+    /** If `true` appends the url inside the link's href to the ping's URL query (ie.: `?href=https://example.com`) */
     hrefToQuery?: boolean;
+    /** If `true` only external links will be modified (defaults to `false`) */
     onlyExternal?: boolean;
+    /** Watch DOM changes to modify newly created <a> tags (defaults to `true`) */
+    watchDOM?: boolean;
+    /** Element or elements to watch for changes (defaults to `document.body`) */
+    elementsToWatch?: HTMLElement | HTMLElement[] | Node | Node[] | NodeList | NodeListOf<HTMLElement>;
 }
 
 export class PingTracker {
     config?: PingTrackerConfig;
     urls: string[];
+    #watchedElements?: HTMLElement | HTMLElement[] | Node | Node[] | NodeList | NodeListOf<HTMLElement>;
+    #watchDOM: boolean = true;
+    #onlyExternal: boolean = false;
+    #hrefToQuery: boolean = false;
+    #observer?: MutationObserver;
 
     constructor(url: string);
     constructor(urls: string[]);
@@ -21,18 +39,104 @@ export class PingTracker {
         }
 
         this.config = config;
+        this.#setConfigDefaults();
 
         document.addEventListener("DOMContentLoaded", () => {
-            this.addPing();
+            this.addPingToAllLinks();
+            this.#watchDOMChanges();
         });
     }
 
-    addPing() {
+    addPingToAllLinks() {
         const links = document.querySelectorAll(`a`);
         if (!links) return;
 
-        links.forEach((link: HTMLAnchorElement) => {
-            link.setAttribute(`ping`, this.urls.join(" "));
+        this.addPingToLinks(links);
+    }
+
+    addPingToLinks(links: NodeListOf<HTMLAnchorElement> | HTMLAnchorElement[]) {
+        links.forEach(el => {
+            this.addPingToLink(el);
         });
+    }
+
+    addPingToLink(link: HTMLAnchorElement) {
+        if (link.hasAttribute("ping")) return;
+
+        const href = link.getAttribute("href");
+
+        if (this.#onlyExternal) {
+            if (href) {
+                if (!this.isExternal(href)) {
+                    return;
+                }
+            }
+        }
+
+        const urls: string[] = this.urls;
+        if (this.#hrefToQuery) {
+            urls.forEach(url => {
+                if (url.includes("?")) {
+                    return (url += `&href=${href}`);
+                }
+
+                return (url += `?href=${href}`);
+            });
+        }
+
+        link.setAttribute("ping", urls.join(""));
+    }
+
+    isExternal(url: string) {
+        return new URL(url).origin !== location.origin;
+    }
+
+    #setConfigDefaults() {
+        if (this.config === undefined) return;
+
+        if (this.config.watchDOM === undefined) {
+            this.#watchDOM = true;
+        } else {
+            this.#watchDOM = this.config.watchDOM;
+        }
+
+        if (this.#watchDOM) {
+            if (this.config.elementsToWatch === undefined) {
+                this.#watchedElements = document.body;
+            } else {
+                this.#watchedElements = this.config.elementsToWatch;
+            }
+        }
+
+        this.#onlyExternal = !!this.config.onlyExternal;
+        this.#hrefToQuery = !!this.config.hrefToQuery;
+    }
+
+    #watchDOMChanges() {
+        if (!this.#watchDOM || !this.#watchedElements) return;
+
+        if (this.#watchedElements instanceof Array || this.#watchedElements instanceof NodeList) {
+            this.#watchedElements.forEach(el => {
+                const options: MutationObserverInit = {
+                    subtree: true,
+                    childList: true,
+                };
+
+                const callback = (mutationList: MutationRecord[]) => {
+                    for (const mutation of mutationList) {
+                        if (mutation.type === "childList") {
+                            mutation.addedNodes.forEach(node => {
+                                if (node.nodeName.toLowerCase() === "a" && node instanceof HTMLElement) {
+                                    this.addPingToLink(node as HTMLAnchorElement);
+                                }
+                            });
+                        }
+                    }
+                };
+
+                this.#observer = new MutationObserver(callback);
+                this.#observer.observe(el, options);
+            });
+        }
     }
 }
